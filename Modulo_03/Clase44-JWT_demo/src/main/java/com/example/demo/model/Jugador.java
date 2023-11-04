d6a5a29d858762fe2712cb7c256ab517ffff46b8
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-11-04 16:52:22 -0300

    Clase44-JWT_demo

---
+++ Modulo_03/Clase44-JWT_demo/src/main/java/com/example/demo/model/Jugador.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+public class Jugador {
+    private String nombre;
+    private Integer numeroCamiseta;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNumeroCamiseta() {
+        return numeroCamiseta;
+    }
+
+    public void setNumeroCamiseta(Integer numeroCamiseta) {
+        this.numeroCamiseta = numeroCamiseta;
+    }
+}
